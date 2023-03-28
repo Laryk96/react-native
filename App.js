912e66650e1ab0345ea5f1a7c1e073c80d291179
Lary <lary96bk@gmail.com>
2023-03-28 19:22:43 +0300

    fix

--- App.js
+++ App.js
@@ -1,106 +1,20 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
+
+import { NavigationContainer } from '@react-navigation/native'
 
 import { useFonts } from 'expo-font'
-import {
-	StyleSheet,
-	View,
-	TouchableWithoutFeedback,
-	Keyboard,
-	KeyboardAvoidingView,
-	Platform,
-	ImageBackground,
-	Dimensions,
-} from 'react-native'
-import LoginScreen from './Screens/LoginScreen'
-import RegistrationScreen from './Screens/RegistrationScreen'
+import { useRoute } from './router'
 
 export default function App() {
-	const [showRegistration, setShowRegistration] = useState(true)
-	const [isShowKeyboard, setIsShowKeyboard] = useState(false)
-	const [dimensions, setDimensions] = useState(
-		() => Dimensions.get('window').width
-	)
-
+	const routing = useRoute(null)
 	const [fontsLoaded] = useFonts({
 		'Roboto-Bold': require('./assets/fonts/Roboto-Bold.ttf'),
 		'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
 	})
 
-	useEffect(() => {
-		const onChange = () => {
-			const width = Dimensions.get('window').width
-
-			setDimensions(width)
-		}
-
-		Dimensions.addEventListener('change', onChange)
-		return () => Dimensions.removeEventListener('change', onChange)
-	}, [])
-
-	const changeForm = () => setShowRegistration(prevState => !prevState)
-
-	const keyboardHide = () => {
-		setIsShowKeyboard(false)
-		Keyboard.dismiss()
-	}
-
-	const onSubmit = values => {
-		console.log(values)
-		Keyboard.dismiss()
-
-		setIsShowKeyboard(false)
-	}
-
 	if (!fontsLoaded) {
 		return null
 	}
 
-	return (
-		<TouchableWithoutFeedback onPress={keyboardHide}>
-			<View style={styles.container}>
-				<ImageBackground
-					style={styles.image}
-					source={require('./assets/images/bgd.jpg')}
-				>
-					<KeyboardAvoidingView
-						style={{
-							...styles.wrapper,
-							width: dimensions,
-							marginBottom: isShowKeyboard ? 60 : 0,
-						}}
-						behavior={Platform.OS == 'ios' ? 'padding' : 'height'}
-					>
-						{showRegistration ? (
-							<RegistrationScreen
-								KeyboardApi={{ isShowKeyboard, setIsShowKeyboard }}
-								changeForm={changeForm}
-								submit={onSubmit}
-							/>
-						) : (
-							<LoginScreen
-								KeyboardApi={{ isShowKeyboard, setIsShowKeyboard }}
-								changeForm={changeForm}
-								submit={onSubmit}
-							/>
-						)}
-					</KeyboardAvoidingView>
-				</ImageBackground>
-			</View>
-		</TouchableWithoutFeedback>
-	)
+	return <NavigationContainer>{routing}</NavigationContainer>
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-	},
-	image: {
-		flex: 1,
-		resizeMode: 'cover',
-		justifyContent: 'flex-end',
-	},
-	wrapper: {
-		backgroundColor: '#fff',
-		paddingBottom: 1,
-	},
-})
---
+++ Screens/main/PostScreen.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+export const PostScreen = () => {
+	return (
+		<View style={styles.container}>
+			<Text>PostScreen</Text>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+})
